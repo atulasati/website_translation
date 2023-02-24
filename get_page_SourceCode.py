a44303e94066f3ad468e89f9b9343d3d44f5d577
atul <atulasati@users.noreply.github.com>
2023-02-24 20:08:21 +0530

    Update get_page_SourceCode.py

--- get_page_SourceCode.py
+++ get_page_SourceCode.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 from deep_translator import GoogleTranslator
-from newt import get_translate
 
 
 def getdata(url):
@@ -15,20 +13,17 @@ def getdata(url):
 
 def get_href(html):
         soup = BeautifulSoup(html,'html.parser')
-        link = { 'hrefs_with_http' : [],
-                 'hrefs_without_http' : []
-                 }
+        link = []
         
         for a in soup.find_all('a', href=True):
                 h = a['href']
                 if h.startswith('http'):
-                        link['hrefs_with_http'].append(h)
+                        link.append(h)
 
                 else:
-                        link['hrefs_without_http'].append(h)    
+                        link.append("https://www.classcentral.com"+h)
         return link
 
-url = "https://www.classcentral.com/"
 
 def get_content(html):
         soup = BeautifulSoup(html, 'html.parser')
@@ -39,13 +34,47 @@ def get_content(html):
                 content.append(c)
         return content
 
+def get_translate(url,i):
+    headers = {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+                    }
+    response = requests.get(url,headers=headers)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    print("Html page translation started================")
+    exclude_tags = ['script', 'style', 'comment','meta']
+    for tag in soup.find_all(lambda tag: tag.name not in exclude_tags):
+        if tag.name in ['script', 'style','comment','meta']:
+            continue
+        else:
+            try:
+                if tag.contents:
+                    for child in tag.contents:
+                        if isinstance(child, str):
+                            translated_text = GoogleTranslator(source='en', target='hi').translate(child)
+                            child.replace_with(translated_text)
+                else:
+                    translated_text = GoogleTranslator(source='en', target='hi').translate(tag.get_text())
+                    tag.string.replace_with(translated_text)
+            except:
+                pass
+        
+    translated_html = str(soup)
+    file_path = f"/home/hp/Documents/translate_demo/templates/translated_{i}.html"
+    with open(file_path, 'w') as f:
+        f.write(translated_html)
+        # print("file translated successfully.........!!!!!")
 
-html = getdata(url)
-time.sleep(3)  
-href = get_href(html=html)
 
-get_translate(url,'index')
-for h in range(5): #len(href['hrefs_with_http'])
-        url = href['hrefs_with_http'][h]
-        get_translate(url=url,i=h)
+
+
+def start(count):
+        url = "https://www.classcentral.com/"
+        hrefs = [url]
+        base_hrefs = get_href(html=getdata(url)) 
+        hrefs.extend(base_hrefs)
+        for href in range(count): 
+                url = hrefs[href]
+                get_translate(url=url,i=href)
 
